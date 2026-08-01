1e1f83e68bfd074a5c32753ed5cd4051e5ac338e
ercansavas <ercansavas1@gmail.com>
2026-08-01 20:31:50 +0300

    chore(timeflow): release 0.4.13

--- Casks/timeflow.rb
+++ Casks/timeflow.rb
@@ -1,6 +1,6 @@
 cask "timeflow" do
-  version "0.4.12"
-  sha256 "c74fbfd577e8fa38d3e1728af88f4bbcd3fe11940d6008b7608a9027e7906ecf"
+  version "0.4.13"
+  sha256 "1a30110195689ced653d927aa1e4b016c3566be6655f4db716e457e00bf3216f"
 
   url "https://github.com/ercansavas/homebrew-tap/releases/download/v#{version}/TimeFlow.dmg",
       verified: "github.com/ercansavas/homebrew-tap/"
